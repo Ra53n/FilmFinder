2e839557f7f919d048d449cbb0f3a374505dfd3b
nikkon2001@mail.ru <nikkon2301>
2022-05-02 13:28:06 +0500

    Добавил возможность фильтровать по рейтингу понравившиеся фильмы

--- app/src/main/java/com/example/filmfinder/data/repository/localRepo/RepositoryLocalLikedMovies.kt
+++ app/src/main/java/com/example/filmfinder/data/repository/localRepo/RepositoryLocalLikedMovies.kt
@@ -5,6 +5,7 @@ import com.example.filmfinder.data.room.likedMovies.LikedMoviesEntity
 
 interface RepositoryLocalLikedMovies {
     fun getAllLikedMovies():List<Movie>
+    fun getLikedMoviesSortedByRating(isAsc : Int):List<Movie>
     fun saveMovie(movie: Movie)
     fun deleteMovie(movie: Movie)
     fun deleteMovie(entity: LikedMoviesEntity)
--- app/src/main/java/com/example/filmfinder/data/room/likedMovies/LikedMoviesDao.kt
+++ app/src/main/java/com/example/filmfinder/data/room/likedMovies/LikedMoviesDao.kt
@@ -1,6 +1,7 @@
 package com.example.filmfinder.data.room.likedMovies
 
 import androidx.room.*
+import java.time.LocalDate
 
 @Dao
 interface LikedMoviesDao {
@@ -15,4 +16,12 @@ interface LikedMoviesDao {
 
     @Query("SELECT * FROM liked_movies_entity")
     fun getAllLikedMovies(): List<LikedMoviesEntity>
+
+    @Query("SELECT * FROM liked_movies_entity ORDER BY " +
+            "CASE WHEN :isAsc = 1 THEN rating END ASC, " +
+            "CASE WHEN :isAsc = 2 THEN rating END DESC ")
+    fun getAllMoviesSortedByRating(isAsc : Int): List<LikedMoviesEntity>
+
+    @Query("SELECT * FROM liked_movies_entity")
+    fun getAllUpcomingMovies(): List<LikedMoviesEntity>
 }
